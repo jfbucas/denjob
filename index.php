751d73061b9bd8b48fd57a9ef136af5c54376ee9
JF Bucas <jfbucas@stp.dias.ie>
2017-09-18 21:22:39 +0100

    pre 400px

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ foreach ($positions as $p) {
 		$jobtitle = get_jobtitle($p);
 		$jobdesc  = get_jobdesc($p);
 		echo "<h3>" . $jobtitle . "</h3>\n";
-		echo "<pre width=80>" . $jobdesc . "</pre>\n";
+		echo "<div style='width:500px;overflow:auto'><pre>" . $jobdesc . "</pre></div>\n";
 
 		echo "<br><br>\n";
 		echo "To Apply, please fill the following:\n <br>";
